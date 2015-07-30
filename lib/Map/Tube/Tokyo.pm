495a292564be834edd890fdee5691f31c21bb328
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2015-07-30 10:51:05 +0100

    - Upgraded to use Map::Tube v3.00.

--- lib/Map/Tube/Tokyo.pm
+++ lib/Map/Tube/Tokyo.pm
@@ -1,6 +1,6 @@
 package Map::Tube::Tokyo;
 
-$Map::Tube::Tokyo::VERSION   = '0.19';
+$Map::Tube::Tokyo::VERSION   = '0.20';
 $Map::Tube::Tokyo::AUTHORITY = 'cpan:MANWAR';
 
 =encoding utf8
@@ -11,7 +11,7 @@ Map::Tube::Tokyo - Interface to the Tokyo Subway Map.
 
 =head1 VERSION
 
-Version 0.19
+Version 0.20
 
 =cut
 
